e46cf0c251ede98d58eab9375dcb1ea5660903a6
weiaijie <weiaijie@Outlook.com>
2018-12-20 17:38:30 +0800

    商品详情页模版更新

--- src/views/GoodsPage.vue
+++ src/views/GoodsPage.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="GoodsPage">
 	<HeadTop/>
-    <router-link to="/ip_cat">ip_cat</router-link>
+	<div class="goods">
+		<img src="http://192.168.0.117/src/images/20181204/729b2caf6c398feb814728f01ca95016.jpg" alt="">
+		<p>【玻尿酸去黑眼圈】【铂金瑞蓝玻尿酸1ml】风靡中国，变美不留痕迹(支持现场验真伪，限购1支)</p>
+		<div class="pay">
+			<span>¥680<s>原价: ¥1680</s></span>
+			<p>已预约：3991</p>
+		</div>
+		<div class="tip1">
+			<img src="../assets/tip_01.png" alt="" class="tip_0">
+			<ul>
+				<li>项目</li>
+				<li><span>•</span>玻尿酸</li>
+				<li>时间</li>
+				<li><span>•</span>支付后30天内到院</li>
+				<li>提示</li>
+				<li><span>•</span>小仙女们注意啦~ 此商品为热玛吉第四代（黄金治疗头）：全面部活动价38000</li>
+			</ul>
+			<img src="../assets/doctor_01.png" alt="医生" class="tip_0" style="margin-bottom:0">
+		</div>
+		<div class="content">
+			<img src="../assets/tip_02.png" alt=""  class="tip_0">
+		</div>
+		
+	</div>
+    
     <Footer/>
   </div>
 </template>
@@ -25,7 +49,47 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.goods p{
+	padding:2% 4%;
+	font-size:1.3rem;
+	text-align: left;
+}
+.goods .pay span{
+	float:left;
+	display: block;
+	width: 60%;
+	padding-left:6%;
+	font-size: 2.5rem;
+	color:red;
+	text-align: left;
+}
+.goods .pay s{
+	padding-left:12%;
+	font-size: 1.3rem;
+	color:#3b3b3b;
+}
+.goods .pay p{
+	float:left;
+	padding:2% 0;
+	width: 27%;
+	font-size:1.3rem;
+	text-align: right;
+}
+.goods .tip_0{
+	margin: 1.5rem 0;
+}
+.goods .tip1 ul{
+	width: 80%;
+	margin: auto;
+}
+.goods .tip1 ul li{
+	text-align: left;
+	font-size: 1.3rem;
+	line-height: 2rem;
+}
+.goods .tip1 ul li:nth-child(even) span{
+  color:#ff5571;
+  margin-right:1rem;
+}
 </style>
